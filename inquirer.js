dcb8321f24157ed3cef0838246c2d7b077469e35
Mat Wilmot <matwilmot90@gmail.com>
2020-08-03 16:20:50 -0700

    Added crust question

--- inquirer.js
+++ inquirer.js
@@ -4,6 +4,8 @@ let Pizza;
 
 // Let's decide what sizes of pizza we offer
 const sizesArray = ["10 inch", "12 inch", "14 inch", "16 inch"];
+// Let's decide what crust types we offer
+const crustArray = ["Thin Crust", "Regular", "Pan", "Stuffed Crust"];
 // Let's decide what sauces we offer
 const saucesArray = ["Marinara", "BBQ", "Alfredo Sauce"];
 // Let's decide what toppings we offer
@@ -92,6 +94,23 @@ const getSize = () => {
     ])
     .then((res) => {
       Pizza.size = res.pizza_size;
+      console.log(Pizza);
+      getCrust();
+    });
+};
+
+const getCrust = () => {
+  inquirer
+    .prompt([
+      {
+        name: "pizza_crust",
+        type: "list",
+        message: "Crust type:",
+        choices: crustArray,
+      },
+    ])
+    .then((res) => {
+      Pizza.crust = res.pizza_crust;
       console.log(Pizza);
     });
 };
